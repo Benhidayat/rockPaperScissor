1e555c7aa0b5b45230c21b3b69642ff356c4688e
Benny Hidayat <bennyhidayat141@gmail.com>
2023-02-20 21:17:06 +0700

    created a function so that a game last for 5 rounds. get input from player using window.promt(); show the winner after game ended.

--- external.js
+++ external.js
@@ -9,24 +9,50 @@ function getComputerChoice() {
         return "scissor";
     }
 }
+
+let playerPoint = 0;
+let computerPoint = 0;
+
 //create a function for single round game.
 function singleRound (playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return `Tie! ${playerSelection} equals ${computerSelection}`;
     } else if (playerSelection == "rock" && computerSelection == "paper") {
+        computerPoint += 1;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection == "rock" && computerSelection == "scissor") {
+        playerPoint += 1;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerPoint += 1;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection == "paper" && computerSelection == " scissor") {
+        computerPoint += 1;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection  == "scissor" && computerSelection == "rock") {
+        computerPoint += 1;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     } else {
+        playerPoint += 1;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     }
 }
 
-const playerChoice = "rOck";
-console.log(singleRound(playerChoice.toLowerCase(), getComputerChoice()));
+
+
+//create a function to make a game last for 5 rounds.
+function game() {
+    for ( let i = 0; i < 5; i++) {
+        const playerChoice = prompt("Rock, Paper or Scissor?:", "");
+        console.log(singleRound(playerChoice.toLowerCase(), getComputerChoice()));
+    }
+    if (playerPoint > computerPoint) {
+        return `Winner : Player`;
+    } else if (playerPoint < computerPoint) {
+        return `Winner : Computer`;
+    } else {
+        return `Deuce!`;
+    }
+}
+
+console.log(game());
